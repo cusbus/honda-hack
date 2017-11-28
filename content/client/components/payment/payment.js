c0a7e74a22c32525543d252e401424b13a137050
Justin Caryk <cary9983@gmail.com>
2017-11-27 19:24:19 -0800

    payment not complete.

--- content/client/components/payment/payment.js
+++ content/client/components/payment/payment.js
@@ -1,44 +1,39 @@
 (function () {
     'use strict'
 
-    angular.module('client.home').component('paymentComponent', {
-        templateUrl: '/client/components/payment/payment.html',
-        controller: 'paymentController as ctrl'
-    })
+    angular.module('client.components')
+        .component('paymentComponent', {
+            templateUrl: '/client/components/payment/payment.html',
+            controller: PaymentController
+        })
 
-    angular.module('client.home')
-        .controller('paymentController', PaymentController)
+    PaymentController.$inject = ['$http', '$log', '$alert']
+    function PaymentController($http, $log, $alert) {
+        let $ctrl = this
+        $ctrl.submit = _submit
 
-    PaymentController.$inject('$http', '$log', '$alert', 'shuttleService')
-
-    function PaymentController($http, $log, $alert, shuttleService) {
-        let vm = this
-        vm.submit = _submit
-        
         init()
 
-        function init(){
+        function init() {
             $log.log('payment landed')
         }
 
-        function _submit(){
+        function _submit() {
             let reservation = {
-                name: vm.name,
-                cardType: vm.cardType,
-                cardNumber: vm.cardNumber,
-                cardExpiration: vm.cardExpirationMonth+vm.cardExpirationYear,
-                cardSecCode: vm.cardSecCode,
-                addressA: vm.addressA,
-                addressB: vm.addressB,
-                city: vm.city,
-                state: vm.state,
-                zip: vm.zip
+                name: $ctrl.name,
+                cardType: $ctrl.cardType,
+                cardNumber: $ctrl.cardNumber,
+                cardExpiration: $ctrl.cardExpirationMonth+"|"+$ctrl.cardExpirationYear,
+                cardSecCode: $ctrl.cardSecCode,
+                addressA: $ctrl.addressA,
+                addressB: $ctrl.addressB,
+                city: $ctrl.city,
+                state: $ctrl.state,
+                zip: $ctrl.zip
             }
             $alert(reservation)
             // shuttleService.submitPayment(reservation)
             //     .then(result => $alert('Reservation Completed!'))
         }
-
     }
-
 })()
