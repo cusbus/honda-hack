b837601877315b574976268ac16c77055d608556
James Matheu <james.matheu@gmail.com>
2017-11-27 16:37:57 -0800

    basic theme

---
+++ content/client/home/home.controller.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    angular.module('client.home')
+        .controller('homeController', HomeController)
+    HomeController.$inject = []
+    function HomeController() {
+        var $ctrl = this;
+    }
+})();
