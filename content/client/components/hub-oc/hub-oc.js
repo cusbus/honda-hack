035a2a1397f15770d90ae48eb9326c0f23a2784a
Dianne Saba <dnsaba19@gmail.com>
2017-11-28 07:05:02 -0800

    added cost component

--- content/client/components/hub-oc/hub-oc.js
+++ content/client/components/hub-oc/hub-oc.js
@@ -13,6 +13,12 @@ angular.module('client.crud').component('hubOc',{
 
     function HubOcController($log) {
         const vm = this
+        vm.hub = "Orange County"
+        vm.rates = {
+        metroB: '135.95',
+        metroP: '195.95',
+        metroSP: '225.95'
+        }
         vm.$onInit = _init
  
         function _init() {
