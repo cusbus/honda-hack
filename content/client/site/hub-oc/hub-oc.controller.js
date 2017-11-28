7c2e1656828e1b5c1587783ef545e854fcba9797
Dianne Saba <dnsaba19@gmail.com>
2017-11-27 16:25:45 -0800

    made oc hub

---
+++ content/client/site/hub-oc/hub-oc.controller.js
@@ -0,0 +1,20 @@
+(function() {
+    'use strict';
+
+    angular.module('client.site')
+        .controller('hubOcController', HubOcController)
+
+    HubLaController.$inject = ['$log']
+
+    function HubLaController($log) {
+        var vm = this
+
+        init()
+
+        function init() {
+
+        }
+
+
+    }
+})();
