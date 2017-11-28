77c6417df8c64eaa9ceecd23d16dd11702893531
Dat Tram <dattram0610@gmail.com>
2017-11-28 11:02:29 -0800

    Fixed small errors

--- content/client/hubs/hub-la/hub-la.controller.js
+++ content/client/hubs/hub-la/hub-la.controller.js
@@ -1,14 +1,13 @@
-(function () {
+(function() {
     'use strict';
 
     angular.module('client.hubs')
         .controller('hubLaController', HubLaController)
 
-    HubLaController.$inject = ['sendGridService','$log']
+    HubLaController.$inject = ['$log']
 
-    function HubLaController(sendGridService, $log) {
+    function HubLaController($log) {
         var vm = this
-        vm.sendEmail = _sendEmail
 
         init()
         vm.hub = "Los Angeles"
@@ -23,12 +22,5 @@
 
 
         }
-
-        function _sendEmail() {
-            sendGridService.sendGrid()
-            .then(data => console.log(`this means you got it right! ${data}`))
-            .catch(data => console.log(`WRONG ${data}`))
-        }
-
     }
 })();
