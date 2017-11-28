f88444ea91e897fa62f5e982ce8b84de5f3adb2f
Justin Caryk <cary9983@gmail.com>
2017-11-27 18:17:36 -0800

    most of payment done

--- content/client/components/payment/payment.js
+++ content/client/components/payment/payment.js
@@ -1,23 +1,43 @@
 (function () {
+    'use strict'
 
-    angular.module('client.site').component('paymentComponent', {
-        templateUrl: 'client/components/payment/payment.html',
+    angular.module('client.home').component('paymentComponent', {
+        templateUrl: '/client/components/payment/payment.html',
         controller: 'paymentController as ctrl'
     })
 
-    angular.module('client.site')
+    angular.module('client.home')
         .controller('paymentController', PaymentController)
 
-    PaymentController.$inject('$http')
+    PaymentController.$inject('$http', '$log', '$alert', 'shuttleService')
 
-    function PaymentController($http) {
-
+    function PaymentController($http, $log, $alert, shuttleService) {
+        let vm = this
+        vm.submit = _submit
+        
         init()
 
         function init(){
-            
+            $log.log('payment landed')
         }
 
+        function _submit(){
+            let reservation = {
+                name: vm.name,
+                cardType: vm.cardType,
+                cardNumber: vm.cardNumber,
+                cardExpiration: vm.cardExpirationMonth+vm.cardExpirationYear,
+                cardSecCode: vm.cardSecCode,
+                addressA: vm.addressA,
+                addressB: vm.addressB,
+                city: vm.city,
+                state: vm.state,
+                zip: vm.zip
+            }
+            $alert(reservation)
+            // shuttleService.submitPayment(reservation)
+            //     .then(result => $alert('Reservation Completed!'))
+        }
 
     }
 
